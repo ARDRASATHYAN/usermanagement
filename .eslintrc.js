eae4cd351671e43987c5ed4a25b9fa903c1977ff
ARDRASATHYAN <sardra9988@gmail.com>
2025-08-01 09:59:21 +0530

    premitter and eslint add for code quality

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'prettier'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off',
+    'no-unused-vars': 'warn',
+    'react/react-in-jsx-scope': 'off', // not needed in React 17+
+  },
+}
